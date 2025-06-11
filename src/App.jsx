12d125719ba178f2a4e0280e805f0ac6bc04011a
Regular124 <166163033+Regular124@users.noreply.github.com>
2025-06-11 17:56:51 +0300

    Add files via upload
    Ура, теперь можно тыкать на картинки и они будут меняться.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ const cardData = [
     description: "there should be text here",
     price: "18.000 рублей",
     Stock: "3",
-    imageUrl: "/pics/75354.webp",
-    MoreInfo: '',
+    imageUrl: ["/pics/75354.webp", "/pics/75354-2.webp", "/pics/75354-3.webp"],
+    MoreInfo: "",
+    id: 1,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -19,8 +20,9 @@ const cardData = [
     description: "there should be text here",
     price: "2.500 рублей",
     Stock: "2",
-    imageUrl: "/pics/75378.webp",
+    imageUrl: ["/pics/75378.webp", "/pics/75378-2.webp", "/pics/75378-3.webp",],
     MoreInfo: "",
+    id: 2,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -30,8 +32,9 @@ const cardData = [
     description: "there should be text here",
     price: "40.000 рублей",
     Stock: "4",
-    imageUrl: "/pics/75190.webp",
+    imageUrl: ["/pics/75190.webp", "/pics/75190-2.webp", "/pics/75190-3.webp"],
     MoreInfo: "",
+    id: 3,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -41,8 +44,9 @@ const cardData = [
     description: "there should be text here",
     price: "20.000 рублей",
     Stock: "1",
-    imageUrl: "/pics/75336.jpg",
+    imageUrl: ["/pics/75336.webp", "/pics/75336-2.webp", "/pics/75336-3.webp"],
     MoreInfo: "",
+    id: 4,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -52,8 +56,9 @@ const cardData = [
     description: "there should be text here",
     price: "15.000 рублей",
     Stock: "4",
-    imageUrl: "/pics/75374.jpg",
+    imageUrl: ["/pics/75374.webp", "/pics/75374-2.webp", "/pics/75374-3.webp"],
     MoreInfo: "",
+    id: 5,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -63,8 +68,9 @@ const cardData = [
     description: "there should be text here",
     price: "18.000 рублей",
     Stock: "8",
-    imageUrl: "/pics/75323.webp",
+    imageUrl: ["/pics/75323.webp", "/pics/75323-2.webp", "/pics/75323-3.webp"],
     MoreInfo: "",
+    id: 6,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -74,8 +80,9 @@ const cardData = [
     description: "there should be text here",
     price: "25.000 рублей",
     Stock: "3",
-    imageUrl: "/pics/75355.webp",
+    imageUrl: ["/pics/75355.webp", "/pics/75355-2.webp", "/pics/75355-3.webp"],
     MoreInfo: "",
+    id: 7,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -85,8 +92,9 @@ const cardData = [
     description: "there should be text here",
     price: "13.000 рублей",
     Stock: "6",
-    imageUrl: "/pics/75393.jpg",
+    imageUrl: ["/pics/75393.webp", "/pics/75393-2.webp", "/pics/75393-3.webp"],
     MoreInfo: "",
+    id: 8,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -96,8 +104,9 @@ const cardData = [
     description: "there should be text here",
     price: "15.000 рублей",
     Stock: "3",
-    imageUrl: "/pics/76156.webp",
+    imageUrl: ["/pics/76156.webp", "/pics/76156-2.webp", "/pics/76156-3.webp"],
     MoreInfo: "",
+    id: 9,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -107,8 +116,9 @@ const cardData = [
     description: "Имеет повреждения коробки.",
     price: "30.000 рублей",
     Stock: "1",
-    imageUrl: "/pics/75315.webp",
+    imageUrl: ["/pics/75315.webp", "/pics/75315-2.webp", "/pics/75315-3.webp"],
     MoreInfo: "",
+    id: 10,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -118,8 +128,9 @@ const cardData = [
     description: "Имеет повреждения коробки.",
     price: "20.000 рублей",
     Stock: "1",
-    imageUrl: "/pics/75394.webp",
+    imageUrl: ["/pics/75394.webp", "/pics/75394-2.webp", "/pics/75394-3.webp"],
     MoreInfo: "",
+    id: 11,
     MorePics: "",
     tags: ["#star_wars"],
     archived: false,
@@ -157,11 +168,12 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [regular124, setregular124] = useState(1);
 
+
   return (
     <div className="app">
       <Header />
       <Assortiment1 />
-      <Series1 />
+      {/* <Series1 /> */}
       <AboutMag1 />
       <InterestingAsf1 />
       <AnotherBusiness1 />
@@ -175,9 +187,9 @@ export default function App() {
   function Header() {
     return (
       <header className="header">
-        <div class="">
+        <div className="">
           <div className="DadoImage">
-            <img src="/pics/Kraft_Dado.jpg" height={100} width={100} className="DadoImage2"></img>
+            <img src="/pics/Kraft_Dado.webp" height={100} width={100} className="DadoImage2"></img>
             <h1>Дадовый капиталистический магазин Lego</h1></div>
           <div className="info">(Документов на товар не предоставляем)</div>
         </div>
@@ -188,11 +200,29 @@ export default function App() {
             <button onClick={() => setregular124(3)}>О магазине </button>
             <button onClick={() => setregular124(4)}>Интересные Факты </button>
             <button onClick={() => setregular124(5)}>Иные услуги</button>
-            <input type="text" name="searchstring" id="searchstring" class="header__search-input" placeholder="Бэкенда нет" autocomplete="off"></input>
+            <input type="text" name="searchstring" id="searchstring" className="header__search-input" placeholder="Бэкенда нет"></input>
           </ul>
         </nav>
       </header >
     );
+  }
+
+
+  function Reklama() {
+    return (
+      <div className="reklama">
+        <img src="/pics/Pirat.webp" width={200} height={200} href="https://pp-international.net"></img>
+        <a href="https://pp-international.net">
+          <p>Сайт междунарожного пиратского интернационала</p></a>
+        <p>Вступай в наши ряды! борись с корпоративным рабством!</p>
+        <p>Вступай в национальную пиратскую партию!</p>
+        <a href="https://pirate-party.ru">
+          <p>Пиратская партия России</p></a>
+        <div><img src="/pics/PPR.webp" width={200} height={100} ></img></div>
+        <div><img src="/pics/rutor.webp" width={200} height={200} ></img></div>
+
+      </div>
+    )
   }
 
   // regular124
@@ -204,57 +234,45 @@ export default function App() {
           <h2>В наличии прямо сейчас:</h2>
           <div className="card-container">
             {cardData.map((item) => (
-              <Card cardObj={item} />
+              <Card cardObj={item} key={item.id} />
             ))}
           </div>
         </div>
       </div>) : ''
   }
 
-  function Reklama() {
-    return (
-      <div className="reklama">
-        <img src="/pics/Pirat.png" width={200} height={200} href="https://pp-international.net"></img>
-        <a href="https://pp-international.net">
-          <p>Сайт междунарожного пиратского интернационала</p></a>
-        <p>Вступай в наши ряды! борись с корпоративным рабством!</p>
-        <p>Вступай в национальную пиратскую партию!</p>
-        <a href="https://pirate-party.ru">
-          <p>Пиратская партия России</p></a>
-        <div><img src="/pics/PPR.png" width={200} height={100} ></img></div>
-        <div><img src="/pics/rutor.jpg" width={200} height={200} ></img></div>
 
-      </div>
-    )
-  }
 
   function Card({ cardObj }) {
+    const [apple, setapple] = useState(0);
+    function changepic() { apple == 2 ? setapple(0) : setapple((prev) => (prev) + 1) }
     return (
-      <div className="card">
-        <a href={cardObj.imageUrl} >
-          <img className="card-image" src={cardObj.imageUrl} alt={cardObj.title} />
+      <div className="card" >
+        <a>
+          <img className="card-image" src={cardObj.imageUrl[apple]} alt={cardObj.title} onClick={() => changepic()} />
         </a>
         <div className="card-content">
           <h2 className="card-title">{cardObj.title}</h2>
           <h1 className="card-title">{cardObj.price}</h1>
           <p className="card-description">{cardObj.description}</p>
           <p className="card-Stock">В наличии: {cardObj.Stock} ед.</p>
-          <button className="Buy"> Buy</button>
+          <button className="Buy">Buy</button>
         </div>
       </div>
     )
   }
 
-  function Series1() {
-    return regular124 == 2 ? (
-      <div className="Table">
-        <div className="card-container">
-          {SeriesData.map((item) => (
-            <Card cardObj={item} />
-          ))}
-        </div>
-      </div>) : ''
-  }
+  // function Series1() {
+  //   return regular124 == 2 ? (
+  //     <div className="Table">
+  //       <div className="card-container">
+  //         {SeriesData.map((item) => (
+  //           <CardcardObj={item} />
+  //         ))}
+  //       </div>
+  //     </div>) : ''
+  // }
+
   function AboutMag1() {
     return regular124 == 3 ? (
       <>
